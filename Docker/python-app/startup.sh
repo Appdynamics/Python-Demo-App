91e343e43f32cc403a7c995f5ca36b077023bf3b
Jeff Morgan <jeff.morgan@appdynamics.com>
2016-01-05 23:51:31 +0000

    Updated Python Docker containers

--- Docker/python-app/startup.sh
+++ Docker/python-app/startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# This is a starup script for the Python Demo app
+# This is a startup script for the Python Demo app
 
 # Set EC2 Region variable
 source /appd/env.sh && sed -i "/^host/c\host = ${CONTROLLER}" /appd/Python-Demo-App/appdynamics.cfg
@@ -13,13 +13,13 @@ source /appd/env.sh && sed -i "/^tier/c\tier = ${TIER_NAME}" /appd/Python-Demo-A
 source /appd/env.sh && sed -i "/^node/c\node = ${NODE_NAME}" /appd/Python-Demo-App/appdynamics.cfg
 sed -i 's/localhost/python_mysql/g' /appd/Python-Demo-App/demo/config.py
 sed -i 's/127.0.0.1/python_postgres/g' /appd/Python-Demo-App/demo/config.py
-chown -R appdynamics.appdynamics /appd
 
 # Setup virtualenv
-su - appdynamics -c "virtualenv /appd/Python-Demo-App/env"
-su - appdynamics -c "/appd/Python-Demo-App/env/bin/pip install --pre appdynamics"
-su - appdynamics -c "/appd/Python-Demo-App/env/bin/pip install --allow-external mysql-connector-python -r /appd/Python-Demo-App/requirements.txt"
-su - appdynamics -c "/appd/Python-Demo-App/env/bin/pip install -r /appd/Python-Demo-App/requirements.txt"
+/usr/local/bin/virtualenv /appd/Python-Demo-App/env
+source /appd/Python-Demo-App/env/bin/activate
+/appd/Python-Demo-App/env/bin/pip install --pre appdynamics
+/appd/Python-Demo-App/env/bin/pip install --allow-external mysql-connector-python -r /appd/Python-Demo-App/requirements.txt
+/appd/Python-Demo-App/env/bin/pip install -r /appd/Python-Demo-App/requirements.txt
 
 # Start services
-su - appdynamics -c "source /appd/Python-Demo-App/start.sh"
+source /appd/Python-Demo-App/start.sh
